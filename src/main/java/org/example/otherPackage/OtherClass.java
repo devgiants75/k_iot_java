0db0bdd2910703dce4211f374f52c7460a06ab7c
devgiants75 <devgiants75@gmail.com>
2025-07-08 10:52:07 +0900

    20250708 chapter07 capsulation

--- src/main/java/org/example/otherPackage/OtherClass.java
+++ src/main/java/org/example/otherPackage/OtherClass.java
@@ -7,4 +7,12 @@ public class OtherClass {
     protected void otherMethod() {
         System.out.println("다른 패키지의 메서드(protected)");
     }
+
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+        System.out.println(publicClass.publicField);
+        publicClass.publicMethod();
+        // 어디서든지 접근 가능한 필드
+        // 어디서든지 접근 가능한 메서드
+    }
 }
