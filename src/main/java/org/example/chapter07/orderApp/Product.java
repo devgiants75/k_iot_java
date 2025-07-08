863baff4f4011b415114a35ba490c4984bd06d79
devgiants75 <devgiants75@gmail.com>
2025-07-08 15:27:15 +0900

    20250708 chapter07 orderApp

---
+++ src/main/java/org/example/chapter07/orderApp/Product.java
@@ -0,0 +1,12 @@
+package org.example.chapter07.orderApp;
+
+/*
+ * Product 클래스
+ *
+ * - Field: name(제품명), price(제품가격)
+ * - Constructor: Product(name, price)
+ * - Method: getName(), getPrice() - getter
+ * 			/ calculateTax(): 세금 계산 메서드(기본 10% 세율)
+ * */
+public class Product {
+}
