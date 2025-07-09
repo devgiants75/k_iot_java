f8c03bd51a6b0dcbf19b9d7e8bc4471d7fd3401d
devgiants75 <devgiants75@gmail.com>
2025-07-09 12:11:15 +0900

    20250708 chapter08 practice

--- src/main/java/org/example/chapter08/G_Practice.java
+++ src/main/java/org/example/chapter08/G_Practice.java
@@ -80,7 +80,38 @@ public class G_Practice {
         // > TropicalFruit가 Fruit를 상속받는 인터페이스
         Fruit[] fruits = { apple, orange, banana, mango };
 
+        for (Fruit fruit: fruits) {
+            fruit.describe();
+            // fruit.tropicalFeature();
 
-        
+            if (fruit instanceof TropicalFruit) {
+                // 다운 캐스팅 - 해당 클래스만이 가지는 고유 메서드에 접근
+                TropicalFruit tropicalFruit = (TropicalFruit) fruit;
+                tropicalFruit.tropicalFeature(); // 열대 과일은 후숙하면 더 맛있습니다.
+            }
+        }
+
+        // A instanceof B
+        // 'A' 객체가 B 클래스(종류)와 같은지 확인
+        class 동물 {}
+        class 고양이 extends 동물 {}
+        class 강아지 extends 동물 {}
+
+        동물 애니멀 = new 동물();
+        동물 나비 = new 고양이();
+        동물 초코 = new 강아지();
+
+        if (애니멀 instanceof 고양이) {
+            System.out.println("실행되지 않음! 부모 객체는 자식 객체에 담을 수 없음!");
+        }
+
+        if (나비 instanceof 동물) {
+            System.out.println("실행됨! 나비는 동물의 일부!");
+        }
+
+        if (나비 instanceof 고양이) {
+            System.out.println("실행됨! 나비는 원래 고양이! 고양이만이 가진 특정 기능을 수행하기를 원함!");
+            고양이 야옹 = (고양이) 나비; // if 조건문 외부에서도 가능! 정상 코드! (다만! 확실하게 검증하고 변환하자!)
+        }
     }
 }
