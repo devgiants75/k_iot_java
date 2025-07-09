d3f829b6a2643ab1447c48d59be7501fcfaed0ba
devgiants75 <devgiants75@gmail.com>
2025-07-09 17:19:36 +0900

    20250708 chapter09 java api

--- src/main/java/org/example/chapter09/JavaTime.java
+++ src/main/java/org/example/chapter09/JavaTime.java
@@ -61,6 +61,11 @@ public class JavaTime {
         // .ofPattern(원하고자 하는 패턴을 문자열로 전달)
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss");
 
+        // cf) 포맷 기호 - 대소문자 구분
+        // y(연도), M(월), d(일) / H(시), h(시), m(분), s(초)
+        // - H는 24시간제
+        // - h는 12시간제
+
         // 변경할 날짜/시간 데이터.format(원하는 포맷 형식);
         System.out.println(example.format(formatter)); // 25-07-09 16:45:12
     }
