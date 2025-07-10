dc545749eb905539495e0df70badc3d507231a23
devgiants75 <devgiants75@gmail.com>
2025-07-10 15:26:28 +0900

    20250710 chapter09_practice User

--- src/main/java/org/example/chapter09_practice/service/UserServiceImpl.java
+++ src/main/java/org/example/chapter09_practice/service/UserServiceImpl.java
@@ -24,7 +24,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User getUserById(int id) {
+    public User getUserById(int id) { // 6
         // 사용자 리스트를 순회하여 해당 id와 일치하는 사용자가 있는 경우 반환
         // +) 해당 id의 사용자가 없는 경우 UserNotFoundException 발생
 
@@ -36,7 +36,8 @@ public class UserServiceImpl implements UserService {
             }
         }
 
-        throw new UserNotFoundException("사용자 ID에 " + id + "값이 없습니다.");
+        // 존재하지 않는 id 값을 요청한 것
+        throw new UserNotFoundException("사용자 ID에 " + id + "값이 없습니다. (조회 실패)");
     }
 
     @Override
@@ -54,5 +55,47 @@ public class UserServiceImpl implements UserService {
         // 사용자 특정 id 값으로 사용자 정보 반환 || 예외 발생
         User user = getUserById(id);
         user.setEmail(newEmail);
+    }
+
+    @Override
+    public void deleteUser(int id) {
+        // 리스트 요소 제거
+        // 리스트.remove(제거할요소);
+        // cf) 제거 할 요소의 타입은 리스트의 요소 타입과 일치!
+
+        User userToDelete = null; // 전체 리스트에서 특정 id와 일치하는 사용자 정보를 담을 변수 선언
+
+        for (User user: userList) {
+            if (user.getId() == id) {
+                userToDelete = user;
+                break; // 반복문의 종료
+
+                // cf) return: 메서드의 종료
+            }
+        }
+
+        if (userToDelete == null) {
+            // 삭제하고자 하는 id의 사용자가 없음!
+            throw new UserNotFoundException("사용자 ID에 " + id + "값이 없습니다. (삭제 실패)");
+        }
+
+        // 삭제 가능
+        userList.remove(userToDelete);
+    }
+
+    @Override
+    public List<User> findUsersByName(String name) {
+        // 반환값을 담을 변수 선언 & 초기화
+        List<User> result = new ArrayList<>();
+
+        for (User user: userList) {
+            // 전체 사용자를 순회하며 매개변수의 이름값과 일치하는 경우 result에 삽입
+            if(user.getName().equals(name)) {
+                // 문자열A.equals(문자열B) - 결과값을 boolean으로 반환
+                result.add(user);
+            }
+        }
+
+        return result;
     }
 }
