7b260edfd3c5156aa5db3b21bb62374dd2159b81
devgiants75 <devgiants75@naver.com>
2025-07-22 15:48:35 +0900

    20250722 chapter14 Stream API

--- src/main/java/org/example/chapter14/K_Practice.java
+++ src/main/java/org/example/chapter14/K_Practice.java
@@ -8,6 +8,8 @@ import lombok.ToString;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 // 직원 데이터 정의
 @AllArgsConstructor // @Setter 역할
@@ -31,17 +33,76 @@ public class K_Practice {
 
         // 1. 급여가 400 이상인 직원 '필터링'
         // : filter() - 조건을 검사해서 true인 요소만을 다시 반환
+        List<Employee> highSalaryEmployees = employees.stream()
+                .filter(e -> e.getSalary() >= 400)
+                .toList();
+
+        System.out.println("=== 1번 ===");
+        highSalaryEmployees.forEach(System.out::println);
 
         // 2. IT 부서 직원의 이름만 추출
         // 1) 부서(department)가 'IT'인 직원을 필터링 - filter
         // 2) 부서가 'IT'인 직원 목록을 전체 순회하여 이름만 추출 - map
+        List<String> itEmployeeNames = employees.stream()
+                .filter(e -> "IT".equals(e.getDepartment()))
+                // .map(e -> e.getName())
+                .map(Employee::getName)
+                .toList();
+
+        System.out.println("=== 2번 ===");
+        System.out.println(itEmployeeNames);
 
         // 3. 특정 부서(HR) 직원 중 이름에 '조'가 포함된 직원 필터링
         // 1) 부서가 'HR'인 직원을 필터링 >> equals
         // 2) 위의 목록 중 이름에 '조'가 포함된 직원을 필터링 >> contains
 
+        List<Employee> employeeWithJo = employees.stream()
+                        .filter(e -> e.getDepartment().equals("HR"))
+                        .filter(e -> e.getName().contains("조"))
+                                .toList();
+
+        System.out.println("=== 3번 ===");
+        employeeWithJo.forEach(System.out::println);
+        employeeWithJo.forEach(e -> System.out.println(e.getName()));
+
+
         // 4. 부서별 직원 그룹화
         // Collectors 클래스 - groupingBy
+        //      >> Map 인터페이스 반환
+        Map<String, List<Employee>> employeeByDept = employees.stream()
+//                .collect(Collectors.groupingBy(e -> e.getDepartment()));
+                .collect(Collectors.groupingBy(Employee::getDepartment));
 
+        System.out.println(employeeByDept);
+
+        for (Map.Entry<String, List<Employee>> entry: employeeByDept.entrySet()) {
+            String department = entry.getKey();
+            List<Employee> employeesInDept = entry.getValue();
+
+            System.out.print(department + ": ");
+            for (Employee employee: employeesInDept) {
+                System.out.println(employee.getName() + " ");
+            }
+
+            System.out.println();
+        }
+
+        employeeByDept.forEach((department, employeeInDept) -> {
+            String name = employeeInDept.stream()
+                    .map(Employee::getName)
+                    .collect(Collectors.joining(" "));
+
+            System.out.println(department + ": " + name);
+        });
+
+        // 5. 부서별(그룹화) 평균 급여 계산
+        // Collectors 클래스 - groupingBy
+        //                  - averaging타입: 해당 타입의 평균을 계산 (averagingInt(각 직원의 급여 전달))
+        Map<String, Double> avgSalaryByDept = employees.stream()
+                .collect(Collectors.groupingBy(
+                        Employee::getDepartment, Collectors.averagingInt(Employee::getSalary)
+                ));
+
+        System.out.println(avgSalaryByDept);
     }
 }
