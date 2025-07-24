b7b6fcd1bc0a217bd8b84fff51a60a57c3e6a6ac
devgiants75 <devgiants75@naver.com>
2025-07-24 14:14:59 +0900

    20250724 user_reservation

--- src/main/java/org/example/user_reservation/entity/Reservation.java
+++ src/main/java/org/example/user_reservation/entity/Reservation.java
@@ -1,4 +1,21 @@
 package org.example.user_reservation.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Date;
+
+// final 또는 @NonNull인 필드만 생성자의 매개변수로 작성
+@RequiredArgsConstructor
+@Getter
 public class Reservation {
+    private final Long reservationId; // 각 예약의 고유값
+    private final String userId; // 예약자 정보
+    private final Date reservationTime; // 예약 시간
+
+    // 활성화 유무를 저장
+    private boolean isActive = true;
+
+
+
 }
