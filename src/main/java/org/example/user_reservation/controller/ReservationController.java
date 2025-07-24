4170dcb79192fb5164150f869065b94a39a8adf1
devgiants75 <devgiants75@naver.com>
2025-07-24 15:53:02 +0900

    20250724 user_reservation

--- src/main/java/org/example/user_reservation/controller/ReservationController.java
+++ src/main/java/org/example/user_reservation/controller/ReservationController.java
@@ -1,4 +1,41 @@
 package org.example.user_reservation.controller;
 
+import org.example.user_reservation.service.ReservationService;
+import org.example.user_reservation.service.UserService;
+import org.example.user_reservation.service.implement.ReservationServiceImpl;
+import org.example.user_reservation.service.implement.UserServiceImpl;
+
+import java.util.Scanner;
+
 public class ReservationController {
+    private final UserService userService;
+    private final ReservationService reservationService;
+
+    public ReservationController() {
+        this.userService = new UserServiceImpl();
+        this.reservationService = new ReservationServiceImpl(userService);
+    }
+
+    public void run() {
+
+    }
+
+    private void displayMenu() {
+        System.out.println("1. 사용자 등록 | 2. 로그인 | 3. 로그아웃");
+        System.out.println("4. 예약 등록 | 5. 예약 확인 | 6. 예약 취소");
+        System.out.println("7. 종료");
+        System.out.print("선택 >> ");
+    }
+
+    private int getChoice(Scanner sc) {
+        while (!sc.hasNextInt()) {
+            // hasNextInt()
+            // : 입력값이 int면 true, 그렇지 않으면 false 반환
+
+            // 입력값이 정수가 아닌 경우
+            System.out.println("숫자를 입력해주세요.");
+            sc.next();
+        }
+        return sc.nextInt();
+    }
 }
--- src/main/java/org/example/user_reservation/service/UserService.java
+++ src/main/java/org/example/user_reservation/service/UserService.java
@@ -1,4 +1,11 @@
 package org.example.user_reservation.service;
 
+import org.example.user_reservation.dto.UserLoginRequestDto;
+import org.example.user_reservation.dto.UserRegisterRequestDto;
+
 public interface UserService {
+    void registerUser(UserRegisterRequestDto dto);
+    void login(UserLoginRequestDto dto);
+    void logout();
+    boolean isLoggedIn(); // 사용자의 로그인 유무를 확인
 }
